5513ed16d3c82cc1fc6f7f4126ad61e0c9fcc779
blissful <blissful@sunsetglow.net>
2023-10-26 19:41:59 -0400

    remove unused fixtures and patches

--- conftest.py
+++ conftest.py
@@ -5,7 +5,6 @@ import sqlite3
 from collections.abc import Iterator
 from pathlib import Path
 
-import _pytest.pathlib
 import pytest
 from click.testing import CliRunner
 
@@ -183,43 +182,3 @@ def source_dir(config: Config) -> Path:
     shutil.copytree(TEST_PLAYLIST_1, config.music_source_dir / "!playlists")
     update_cache(config)
     return config.music_source_dir
-
-
-def freeze_database_time(conn: sqlite3.Connection) -> None:
-    """
-    This function freezes the CURRENT_TIMESTAMP function in SQLite3 to
-    "2020-01-01 01:01:01". This should only be used in testing.
-    """
-    conn.create_function(
-        "CURRENT_TIMESTAMP",
-        0,
-        _return_fake_timestamp,
-        deterministic=True,
-    )
-
-
-def _return_fake_timestamp() -> str:
-    return "2020-01-01 01:01:01"
-
-
-# Pytest has a bug where it doesn't handle namespace packages and treats same-name files
-# in different packages as a naming collision. https://stackoverflow.com/a/72366347
-
-resolve_pkg_path_orig = _pytest.pathlib.resolve_package_path
-namespace_pkg_dirs = [str(d) for d in Path(__file__).parent.iterdir() if d.is_dir()]
-
-
-# patched method
-def resolve_package_path(path: Path) -> Path | None:
-    # call original lookup
-    result = resolve_pkg_path_orig(path)
-    if result is None:
-        result = path  # let's search from the current directory upwards
-    for parent in result.parents:  # pragma: no cover
-        if str(parent) in namespace_pkg_dirs:
-            return parent
-    return None  # pragma: no cover
-
-
-# apply patch
-_pytest.pathlib.resolve_package_path = resolve_package_path
