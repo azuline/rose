9adb6c1c49239b33c4563eef18880b1d20e85655
blissful <blissful@sunsetglow.net>
2023-10-28 10:36:29 -0400

    sort tracks properly in get_release when trackno is not zero padded

--- rose/cache.py
+++ rose/cache.py
@@ -1692,7 +1692,7 @@ def get_release(
             JOIN releases r ON r.id = t.release_id
             LEFT JOIN artists a ON a.track_id = t.id
             WHERE r.id = ? OR r.virtual_dirname = ?
-            ORDER BY t.disc_number, t.track_number
+            ORDER BY t.formatted_release_position
             """,
             (release_id_or_virtual_dirname, release_id_or_virtual_dirname),
         )
