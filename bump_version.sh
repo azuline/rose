82ba5d8d3df34233e7be210d8b13334187487229
blissful <blissful@sunsetglow.net>
2023-11-19 21:11:20 -0500

    add bump version script

---
+++ bump_version.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+version="$1"
+message="$2"
+
+[ -z "$version" ] && echo "Must specify a version." && exit 1
+[ -z "$message" ] && echo "Must specify a message." && exit 1
+
+# Fail if the branch is dirty.
+git diff --exit-code
+
+echo "$version" > "$ROSE_ROOT/rose/.version"
+git add .
+git commit -am "[Release] $version - $message"
+git tag "$version" HEAD -m "$version"
+git push origin "$version"
+git checkout release
+git reset --hard "$version"
+git push --force
