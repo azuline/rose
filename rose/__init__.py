4291f1c84381a274d98890021eb3500f05fd660d
blissful <blissful@sunsetglow.net>
2024-03-09 18:05:34 -0500

    include the YYYY-MM-DD date in verbose logs

--- rose/__init__.py
+++ rose/__init__.py
@@ -24,22 +24,25 @@ LOG_EVEN_THOUGH_WERE_IN_TEST = os.environ.get("LOG_TEST", False)
 # Add a logging handler for stdout unless we are testing. Pytest
 # captures logging output on its own, so by default, we do not attach our own.
 if "pytest" not in sys.modules or LOG_EVEN_THOUGH_WERE_IN_TEST:  # pragma: no cover
-    stream_template = "[%(asctime)s] %(levelname)s: %(message)s"
-    verbose_template = "[ts=%(asctime)s.%(msecs)03d] [pid=%(process)d] [src=%(name)s:%(lineno)s] %(levelname)s: %(message)s"
-
-    stream_formatter = logging.Formatter(
-        stream_template if not LOG_EVEN_THOUGH_WERE_IN_TEST else verbose_template,
+    simple_formatter = logging.Formatter(
+        "[%(asctime)s] %(levelname)s: %(message)s",
         datefmt="%H:%M:%S",
     )
+    verbose_formatter = logging.Formatter(
+        "[ts=%(asctime)s.%(msecs)03d] [pid=%(process)d] [src=%(name)s:%(lineno)s] %(levelname)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
     stream_handler = logging.StreamHandler(sys.stderr)
-    stream_handler.setFormatter(stream_formatter)
+    stream_handler.setFormatter(
+        simple_formatter if not LOG_EVEN_THOUGH_WERE_IN_TEST else verbose_formatter
+    )
     logger.addHandler(stream_handler)
 
-    file_formatter = logging.Formatter(verbose_template, datefmt="%H:%M:%S")
     file_handler = logging.handlers.RotatingFileHandler(
         LOGFILE,
         maxBytes=20 * 1024 * 1024,
         backupCount=10,
     )
-    file_handler.setFormatter(file_formatter)
+    file_handler.setFormatter(verbose_formatter)
     logger.addHandler(file_handler)
